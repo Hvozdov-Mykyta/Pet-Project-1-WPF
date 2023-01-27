2e691c6ddce8c712fe7ba690055e6ba6d1e1e0c3
Mykyta Hvozdov <mykyta.hvozdov@gmail.com>
2023-01-27 17:59:08 +0200

    Clear, close buttons and main interface created

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,14 +15,36 @@ using System.Windows.Shapes;
 
 namespace Pet_Project_1_WPF
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            Method_ComboBox.SelectedIndex = 0;
+        }
+
+        private void Solve_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Clear_Button_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (TextBox box in TexBoxes_Grid.Children)
+                box.Clear();
+            MaxIterations_TextBox.Clear();
+        }
+
+        private void Close_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var response = MessageBox.Show("Close program?", "Close", MessageBoxButton.YesNo);
+            if (response == MessageBoxResult.Yes)
+                Close();
+        }
+
+        private void Method_ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            MaxIterations_TextBox.Visibility = Method_ComboBox.SelectedIndex == 1 ? Visibility.Visible : Visibility.Hidden;
         }
     }
 }
