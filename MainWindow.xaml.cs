7e449ff42714d50026ddc852eaaad48f3dc1fa26
Mykyta Hvozdov <mykyta.hvozdov@gmail.com>
2023-01-27 18:17:51 +0200

    Created logic of program to search root of equation

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,7 +25,84 @@ namespace Pet_Project_1_WPF
 
         private void Solve_Button_Click(object sender, RoutedEventArgs e)
         {
+            double left, right, accuracy, result;
+            int counter = 0, maxIterations;
 
+            try
+            {
+                left = Convert.ToDouble(Left_TextBox.Text);
+                right = Convert.ToDouble(Right_TextBox.Text);
+                accuracy = Convert.ToDouble(Accuracy_TextBox.Text);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+                return;
+            }
+
+            if (accuracy < 1e-15 || accuracy > 1e-3)
+            {
+                accuracy = 1e-6;
+                Accuracy_TextBox.Text = "1e-6";
+            }
+
+            if (left == right)
+            {
+                MessageBox.Show("Left and right borders must be difference.");
+                return;
+            }
+
+            if (left > right)
+            {
+                (right, left) = (left, right);
+                Left_TextBox.Text = left.ToString();
+                Right_TextBox.Text = right.ToString();
+            }
+
+            if (Math.Abs(RSM.Function(left)) < accuracy)
+            {
+                Root_TextBox.Text = left.ToString();
+                Iterations_TextBox.Text = counter.ToString();
+                return;
+            }
+
+            if (Math.Abs(RSM.Function(right)) < accuracy)
+            {
+                Root_TextBox.Text = right.ToString();
+                Iterations_TextBox.Text = counter.ToString();
+                return;
+            }
+
+            switch (Method_ComboBox.SelectedIndex)
+            {
+                case 0:
+                    {
+                        if (RSM.Function(left) * RSM.Function(right) > 0)
+                        {
+                            MessageBox.Show("There is no SINGLE root on interval");
+                            return;
+                        }
+                        result = RSM.Halving(left, right, accuracy, ref counter);
+                    }
+                    break;
+                case 1:
+                    {
+                        try
+                        {
+                            maxIterations = Convert.ToInt32(MaxIterations_TextBox.Text);
+                            result = RSM.Newton(left, right, accuracy, maxIterations, ref counter);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                            return;
+                        }
+                    }
+                    break;
+                default: return;
+            }
+            Root_TextBox.Text = result.ToString();
+            Iterations_TextBox.Text = counter.ToString();
         }
 
         private void Clear_Button_Click(object sender, RoutedEventArgs e)
@@ -46,5 +123,7 @@ namespace Pet_Project_1_WPF
         {
             MaxIterations_TextBox.Visibility = Method_ComboBox.SelectedIndex == 1 ? Visibility.Visible : Visibility.Hidden;
         }
+
+        private RootSearchMethods RSM = new RootSearchMethods();
     }
 }
---
+++ RootSearchMethods.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Pet_Project_1_WPF
+{
+    internal class RootSearchMethods
+    {
+        public double Halving(double left, double right, double accuracy, ref int counter)
+        {
+            double middle = left;
+            while (Math.Abs(right - left) > accuracy)
+            {
+                middle = (right + left) / 2;
+                counter++;
+                if (Math.Abs(Function(middle)) < accuracy)
+                    break;
+                else if (Function(left) * Function(middle) < 0)
+                    right = middle;
+                else
+                    left = middle;
+            }
+            return middle;
+        }
+
+        public double Newton(double left, double right, double accuracy, int maxIterations, ref int counter)
+        {
+            double delta, x;
+            double derivateWidth = accuracy / 100;
+            if (Function(left) * SecondDerivate(left, derivateWidth) < 0)
+            {
+                if (Function(right) * SecondDerivate(right, derivateWidth) < 0)
+                {
+                    throw new Exception("Convergence of iterations is not guaranted");
+                }
+                x = right;
+            }
+            else
+                x = left;
+            for (int i = 0; i < maxIterations; i++)
+            {
+                delta = Function(x) / Derivate(x, derivateWidth);
+                x -= delta;
+                if (Math.Abs(delta) < accuracy)
+                {
+                    counter = ++i;
+                    return x;
+                }
+            }
+            throw new Exception("No solution was found for the given number of iterations");
+        }
+
+        public double Function(double X)
+        {
+            return X * X - 4;
+        }
+
+
+
+        private double Derivate(double x, double width)
+        {
+            return (Function(x + width) - Function(x)) / width;
+        }
+
+        private double SecondDerivate(double x, double width)
+        {
+            return (Function(x + width) + Function(x - width) - 2 * Function(x)) / (width * width);
+        }
+    }
+}
