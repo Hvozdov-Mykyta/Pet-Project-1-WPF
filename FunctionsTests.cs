312117486faad3d4c39696ef2baea0a58343a36e
Mykyta Hvozdov <mykyta.hvozdov@gmail.com>
2023-02-07 11:40:28 +0200

    Finished Unit Tests

---
+++ FunctionsTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pet_Project_1_WPF
+{
+    internal class FunctionsTests
+    {
+    }
+}
